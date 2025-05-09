3983e9766292cd87da8d1b650ff83a461d466f14
Nicolas <nicolas.gracioli@gmail.com>
2025-05-09 17:14:48 -0300

    feat(aos): integrate aos animation library and configure initial animations

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,6 @@
+import { useEffect } from "react";
+import AOS from "aos";
+import "aos/dist/aos.css";
 import TimeCounter from "./components/TimeCounter";
 import Gallery from "./components/Gallery";
 import LoveMessageBox from "./components/LoveMessageBox";
@@ -9,17 +12,22 @@ import Footer from "./components/Footer";
 import "./index.css";
 
 function App() {
+    useEffect(() => {
+        AOS.init({ duration: 1000 });
+    }, []);
+
     return (
         <div className="min-h-screen bg-pink-50 font-sans flex flex-col">
             <Navbar />
             <div className="h-20" />
-            <div>
+            <div data-aos="fade-down">
                 <WelcomeText />
             </div>
 
             <main className="flex-1 flex flex-col gap-10 md:gap-16 max-w-5xl w-full mx-auto px-2 sm:px-6 md:px-10 py-6">
                 <section
                     className="rounded-3xl bg-white shadow-xl p-6 md:p-10 flex flex-col items-center gap-6 border border-pink-100"
+                    data-aos="fade-up"
                 >
                     <TimeCounter />
                 </section>
