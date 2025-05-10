ce6368c6f0366cb5d1611decf6f4a61e15cf7c99
ngracioli <nicolas.gracioli@gmail.com>
2025-05-09 21:33:39 -0300

    feat(navbar): change navbar text color

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 function Navbar() {
     return (
         <nav className="w-full bg-white shadow-md fixed top-0 left-0 z-30 px-4 py-4 flex items-center justify-center">
-            <span className="text-3xl md:text-4xl font-extrabold text-pink-600 tracking-tight drop-shadow-lg select-none">
+            <span className="text-3xl md:text-4xl font-extrabold text-pink-500 tracking-tight drop-shadow-lg select-none">
                 Luana & Nicolas
             </span>
         </nav>
