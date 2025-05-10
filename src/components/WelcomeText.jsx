e76fd6d807663b74653307c1912f6f2439faca0e
ngracioli <nicolas.gracioli@gmail.com>
2025-05-09 21:34:36 -0300

    feat(welcometext): change text colors

--- src/components/WelcomeText.jsx
+++ src/components/WelcomeText.jsx
@@ -1,10 +1,10 @@
 function WelcomeText() {
     return (
         <section className="flex flex-col items-center justify-center text-center py-10 px-4 md:py-16 md:px-0">
-            <h1 className="text-4xl md:text-5xl font-extrabold text-pink-600 mb-4 drop-shadow-lg">
+            <h1 className="text-4xl md:text-5xl font-extrabold text-pink-500 mb-4 drop-shadow-lg">
                 Luana & Nicolas
             </h1>
-            <p className="italic text-xl md:text-2xl text-pink-500 mb-2">
+            <p className="italic text-xl md:text-2xl text-pink-400 mb-2">
                 Feliz Dia dos Namorados, minha princesa!
             </p>
             <p className="text-lg md:text-xl text-pink-400">
